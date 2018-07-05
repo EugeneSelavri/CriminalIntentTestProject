ae833aa4191062932feebb529facaec8df52ed37
Eugene Selavri <eugene.selavri@gmail.com>
2018-07-05 22:32:33 +0300

    add viewpager

--- app/src/main/java/ru/startandroid/criminalintenttwo/CrimeDetailsActivity.java
+++ app/src/main/java/ru/startandroid/criminalintenttwo/CrimeDetailsActivity.java
@@ -5,12 +5,19 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
 import java.util.UUID;
 
 public class CrimeDetailsActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+    private UUID crimeId;
 
     private static final String EXTRA_CRIME_ID = "ru.startandroid.criminalintenttwo.crime_id";
 
@@ -25,13 +32,14 @@ public class CrimeDetailsActivity extends AppCompatActivity {
         }
 
         if (savedInstanceState == null) {
-            FragmentManager fm = getSupportFragmentManager();
-            Fragment fragment = fm.findFragmentById(R.id.crime_details_container);
 
-            if (fragment == null) {
-                UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
-                fragment = CrimeDetailsFragment.newInstance(crimeId);
-                fm.beginTransaction().add(R.id.crime_details_container, fragment).commit();
+            crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+            pagerSettings();
+            for (int i = 0; i < mCrimes.size(); i++) {
+                if (mCrimes.get(i).getId().equals(crimeId)) {
+                    mViewPager.setCurrentItem(i);
+                    break;
+                }
             }
         }
     }
@@ -40,5 +48,25 @@ public class CrimeDetailsActivity extends AppCompatActivity {
         Intent intent = new Intent(packageContext, CrimeDetailsActivity.class);
         intent.putExtra(EXTRA_CRIME_ID, crimeId);
         return intent;
+    }
+
+    private void pagerSettings() {
+        mViewPager = (ViewPager) findViewById(R.id.crime_details_container);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeDetailsFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
