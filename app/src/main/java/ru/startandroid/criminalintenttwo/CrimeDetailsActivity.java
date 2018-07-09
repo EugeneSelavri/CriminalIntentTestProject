710d67075c7b346d386ea8f829d269c7e03edc2d
Eugene Selavri <eugene.selavri@gmail.com>
2018-07-09 21:16:30 +0300

    add buttons to start and to end

--- app/src/main/java/ru/startandroid/criminalintenttwo/CrimeDetailsActivity.java
+++ app/src/main/java/ru/startandroid/criminalintenttwo/CrimeDetailsActivity.java
@@ -3,12 +3,16 @@ package ru.startandroid.criminalintenttwo;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +22,9 @@ public class CrimeDetailsActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
     private UUID crimeId;
+    private Button mStartButton;
+    private Button mEndButton;
+    private int buttonPos;
 
     private static final String EXTRA_CRIME_ID = "ru.startandroid.criminalintenttwo.crime_id";
 
@@ -35,9 +42,12 @@ public class CrimeDetailsActivity extends AppCompatActivity {
 
             crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
             pagerSettings();
+            buttonSettings();
+            buttonListener();
+
             for (int i = 0; i < mCrimes.size(); i++) {
                 if (mCrimes.get(i).getId().equals(crimeId)) {
-                    mViewPager.setCurrentItem(i);
+                    setCurrentIndex(i);
                     break;
                 }
             }
@@ -55,7 +65,7 @@ public class CrimeDetailsActivity extends AppCompatActivity {
 
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
-        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
 
             @Override
             public Fragment getItem(int position) {
@@ -67,6 +77,63 @@ public class CrimeDetailsActivity extends AppCompatActivity {
             public int getCount() {
                 return mCrimes.size();
             }
+
+            @Override
+            public int getItemPosition(@NonNull Object object) {
+                return super.getItemPosition(object);
+            }
         });
+
+    }
+
+    private void buttonListener() {
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                updateButtonStateAtPosition(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void buttonSettings() {
+        mStartButton = (Button) findViewById(R.id.to_the_start);
+        mEndButton = (Button) findViewById(R.id.to_the_end);
+
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setCurrentIndex(0);
+            }
+        });
+
+
+        mEndButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              setCurrentIndex(mCrimes.size()-1);
+            }
+        });
+
+    }
+
+    private void setCurrentIndex(int index) {
+        mViewPager.setCurrentItem(index);
+        updateButtonStateAtPosition(index);
+    }
+
+    private void updateButtonStateAtPosition(int index) {
+        mStartButton.setEnabled(index > 0);
+        mEndButton.setEnabled(index < mCrimes.size()-1);
     }
 }
